fcf9ace927ffff4f930049c3651d90f717337b1a
Gerry Permana <permanagerry@gmail.com>
2024-09-05 01:29:15 +0700

    fixing test component

--- src/test/java/belajarspringdasar/maven/ComponentTest.java
+++ src/test/java/belajarspringdasar/maven/ComponentTest.java
@@ -1,6 +1,10 @@
 package belajarspringdasar.maven;
 
+import belajarspringdasar.maven.component.repository.SetterFieldBasedRepository;
+import belajarspringdasar.maven.component.repository.SetterRepository;
 import belajarspringdasar.maven.service.ComponentService;
+import belajarspringdasar.maven.service.SetterFieldBasedService;
+import belajarspringdasar.maven.service.SetterService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -24,6 +28,22 @@ public class ComponentTest {
 
         Assertions.assertNotNull(componentService);
         Assertions.assertSame(componentService, componentService1);
+    }
+
+    @Test
+    void testSetterDependencyInjection() {
+        SetterService setterService = applicationContext.getBean(SetterService.class);
+        SetterRepository setterRepository = applicationContext.getBean(SetterRepository.class);
+
+        Assertions.assertSame(setterRepository, setterService.getSetterRepository());
+    }
+
+    @Test
+    void testSetterFieldBasedDependencyInjection() {
+        SetterFieldBasedService setterFieldBasedService = applicationContext.getBean(SetterFieldBasedService.class);
+        SetterFieldBasedRepository fieldBasedRepository = applicationContext.getBean(SetterFieldBasedRepository.class);
+
+        Assertions.assertSame(fieldBasedRepository, setterFieldBasedService.getSetterFieldBasedRepository());
     }
 
 }
